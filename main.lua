cf9607b99dccc8d1595c1e4774b6b39ab8a0842a
Saussereau Dominique <domsau2@yahoo.fr>
2020-11-29 00:16:14 +0100

    Test winner

--- main.lua
+++ main.lua
@@ -4,10 +4,60 @@ function love.conf()
   love.window.borderless = true
   love.window.resizable = true
 end
+function win(board) -- return player win (1 or 2), 0
+  for i = 1, 3 do
+    if (board[i][1] * board[i][2] * board[i][3]  == 1) then
+      return 1
+    end
+  end
+  for j = 1, 3 do
+    if (board[1][j] * board[2][j] * board[3][j]  == 1) then
+      return 1
+    end
+  end
+  for i = 1, 3 do
+    if (board[i][1] + board[i][2] + board[i][3]  == 6) then
+      return 2
+    end
+  end
+  for j = 1, 3 do
+    if (board[1][j] + board[2][j] + board[3][j]  == 6) then
+      return 2
+    end
+
+  end
+    if (board[1][1] + board[2][2] + board[3][3]  == 6) then
+      return 2
+    end
+      if (board[3][1] + board[2][2] + board[1][3]  == 6) then
+      return 2
+    end
+    if (board[1][1] * board[2][2] * board[3][3]  == 1) then
+      return 1
+    end
+      if (board[3][1] * board[2][2] * board[1][3]  == 1) then
+      return 1
+    end
+
+  
+  return 0
+end
+
+
+function winAtStart(board)
+  playerWin = win(board)
+  if (win(board)== 0) then
+
+    return false
+  end
+
+  return true
+end
+
 
 function love.load()
   love.graphics.setBackgroundColor(0.3,0.2, 0.1) 
-   x, y, w, h = 10, 70, 120, 120  
+  x, y, w, h = 10, 70, 120, 120  
   love.graphics.setLineWidth( h/10 )
 -- logo = love.graphics.newImage("Lua-Logo_128x128.png")
 -- logoP = love.graphics.newImage("powered-by-lua.gif") -- GIF not supported!
@@ -17,11 +67,14 @@ function love.load()
   playerToPlay = love.math.random( 2 ) -- 1 or 2
   displayMenu = true
   boardNew = {{0,0,0}, {0,0,0}, {0,0,0}}
-  
+  board = boardNew
+  -- repeat 
   boardRand = {{-1 +love.math.random( 3 ),-1 +love.math.random( 3 ),-1 +love.math.random( 3 )}, {-1 +love.math.random( 3 ),-1 +love.math.random( 3 ),-1 +love.math.random( 3 )}, {-1 +love.math.random( 3 ),-1 +love.math.random( 3 ),-1 +love.math.random( 3 )}} -- 0, 1, or 2 -- Later: more 0 if winAtStart
 -- Too many case of winAtStart == true.
+  board = boardRand
+  --until (winAtStart(board) == true)
 
-board = boardRand
+
   NewFS = "New game from nothing."
   Exercise = "New exercise from random."
 end
@@ -46,24 +99,40 @@ function colorGraphs(player)
     love.graphics.setColor(1, 0, 0) 
   end
   if (player == 2 ) then
-   love.graphics.setColor(0, 0, 1) 
+    love.graphics.setColor(0, 0, 1) 
   end
-  end
+end
 function playerToPlayXO(player)
-if (player == 1) then 
-return "O"
-end
-if (player == 2) then 
-return "X"
-end
+  if (player == 1) then 
+    return "O"
+  end
+  if (player == 2) then 
+    return "X"
+  end
 
 end
 function love.draw()
+  if (win(board)==1) then
+    colorGraphs(1)
+    love.graphics.print("Player 1 O wins!", 10, 10)
+    displayMenu = true
+  end
+  if (win(board)==2) then
+    colorGraphs(2)
+    love.graphics.print("Player 2 X wins!", 10, 10)
+    displayMenu = true
+  end 
+  if (win(board)==0) then
+    displayMenu = false
+    colorBkBoard()
+
+    colorText()
+    love.graphics.print("Player to play: ", 10, 10)
+    colorGraphs(playerToPlay)
+    love.graphics.print(playerToPlayXO(playerToPlay), 100, 10)
+    colorText()
+  end
   colorText()
-  love.graphics.print("Player to play: ", 10, 10)
-  colorGraphs(playerToPlay)
-  love.graphics.print(playerToPlayXO(playerToPlay), 100, 10)
- colorText()
 
   if (displayMenu == true) then
     love.graphics.print(NewFS, 10, 30)
@@ -80,8 +149,8 @@ function love.draw()
     for j = 1, 3 do
       if (board[i][j]==1) then
         colorGraphs(1)
-      
-       -- love.graphics.circle( "fill", (x/2) + i*(w), ( y/2) + (h/2)+ j*h, h/ 3)
+
+        -- love.graphics.circle( "fill", (x/2) + i*(w), ( y/2) + (h/2)+ j*h, h/ 3)
         love.graphics.circle( "fill", x + w*(i-0.5),  y + h*(j-0.5), h/ 3)
         colorBkBoard()
         love.graphics.circle( "fill", x + w*(i-0.5), y + h*(j-0.5), h/ 4)
@@ -89,7 +158,7 @@ function love.draw()
       end
       if (board[i][j]==2) then
         colorGraphs(2)
-       
+
         love.graphics.line(x  - w + (w/5) + (i*w), y -h + (h/4)+(j*h), x - w + (w/5) + (i*w) + (3 * h/5), y -h + (h/4)+(j*h)+ (3 * h/5)) 
         love.graphics.line( x - w + (w/5) + (i*w) + (3 * h/5), y -h + (h/4)+(j*h),x + - w + (w/5) + (i*w), y -h + (h/4)+(j*h)+ (3 * h/5)) 
 
@@ -99,7 +168,6 @@ function love.draw()
     end
 
   end
-
 
   colorBkBoard()
 
